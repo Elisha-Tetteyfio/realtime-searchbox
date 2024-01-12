bd38b4dfc2cbfdf9d18a1ff9f32091c3b7a15e6e
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2024-01-12 08:02:27 +0000

    Add recent search

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -1,15 +1,23 @@
 class SearchRecordsController < ApplicationController
+  before_action :set_user
+
   def index
     @search_records = SearchRecord.all
+    @my_recent_searches = @user.search_records.all.order("created_at DESC").first(5)
   end
 
   def search
     query = params[:query]
-    ip_address = request.remote_ip
-
-    user = User.find_or_create_by(ip_address: ip_address)
-    SearchRecord.create(query: query, user: user)
+    
+    SearchRecord.create(query: query, user: @user)
 
     render json: { status: 'success' }
+  end
+
+  private
+
+  def set_user
+    ip_address = request.remote_ip
+    @user = User.find_or_create_by(ip_address: ip_address)
   end
 end
